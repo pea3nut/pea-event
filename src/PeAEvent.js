e442d421cbfcaa1a10aa8376459dc4bb32636f3d
花生PeA <626954412@qq.com>
2017-09-05 17:58:16 +0800

    test pass

---
+++ src/PeAEvent.js
@@ -0,0 +1,201 @@
+class PeAEvent{
+    /**
+     * recommend extend this class, direct construct will throw an error
+     * @param {boolean} [asTools] - if you really want just construct it, call with true
+     * */
+    constructor(asTools=false){
+        this._events ={};
+        if(new.target===PeAEvent &&asTools!==true){
+            throw new TypeError('PeAEvent must be extend');
+        }
+    };
+    /**
+     * listen a event
+     * @param {string} eventType - "*" means all
+     * @param {function} listener
+     * @param {boolean} once
+     * */
+    on(eventType,listener,{once}={}){
+        if(typeof eventType!=='string'||typeof listener!=='function'){
+            throw new TypeError('illegal call PeAEvent#on',...arguments);
+        }
+        var event =new PeAEvent.Event(listener);
+        this.addEvent(eventType,event);
+
+        if(once ===true){
+            let that =this;
+            event.setExec(function(){
+                that.off(eventType ,listener);
+                return listener(...arguments);
+            });
+        };
+    };
+    /**
+     * listen a event but once
+     * @alias PeAEvent#on(eventType,listener,{once:true})
+     * @param {string} eventType - "*" means all
+     * @param {function} listener
+     * @param {Object} [options]
+     * */
+    one(eventType,listener,options={}){
+        var args =Array.from(arguments);
+        args[2]=arguments[2]||{};
+        args[2].once =true;
+        return this.on(...args);
+    };
+    /**
+     * remove a event listener
+     * note: if you use * in listener, waiter will be remove too
+     * @param {string} eventType
+     * @param {function|string} listener - "*" means all
+     * @return {boolean} success or no
+     * */
+    off(eventType,listener){
+        var event =new PeAEvent.Event(listener);
+        return this.removeEvent(eventType,event);
+    };
+    /**
+     * wait a event
+     * you can add a checker, return a boolean to specify whether to wait
+     * if return a true, promise will resolve
+     * @param {string} eventType - "*" means all
+     * @param {function} [checker] - a checker function
+     * @return {Promise}
+     * */
+    wait(eventType ,checker=()=>true){
+        return new Promise(resolve =>{
+            var symbol = Symbol(`${eventType}.waiter`);
+            var event = new PeAEvent.Event(symbol ,(...args) =>{
+                if(checker(...args)){
+                    this.off(eventType ,symbol);
+                    resolve();
+                };
+            });
+            this.addEvent(eventType ,event);
+        });
+    }
+    /**
+     * return number of listener
+     * @return {Number}
+     * */
+    has(eventType){
+        if(eventType in this._events){
+            return this._events[eventType].length;
+        }else{
+            return 0;
+        }
+    }
+    /**reset event listener*/
+    reset(){this._events ={}};
+    /**
+     * trigger a event, exec this event's listener all
+     * you can overwrite this method to changed you want,
+     * but overwrite method should use PeAEvent#execListener to exec a listener
+     * @param {string} type - event name to trigger
+     * @param {Array} args - passed argument
+     * @return {Promise}
+     * @abstract
+     * */
+    execEventAll(type,args=[]){
+        if(!Array.isArray(args)){
+            throw TypeError('PeAEvent#execEventAll second argument must be a array');
+        };
+        var promises =[];
+        if(type in this._events){
+            promises =this._events[type]
+                .map(ce=>{
+                    var val =this.execListener(ce.getExec(),args,type);
+                    if([undefined,null].includes(val)){
+                        return null;
+                    }else{
+                        return val;
+                    };
+                })
+                .filter(v=>v!==null)
+            ;
+        };
+        if('*' in this._events &&type!=='*'){
+            this.execEventAll('*' ,...Array.from(arguments).slice(1));
+        }
+        if(promises.length===0)return Promise.resolve();
+        else                   return Promise.race(promises);
+    };
+    /**
+     * how call a event listener
+     * you can overwrite this method to changed you want
+     * @param {function} listener - event listener
+     * @param {Array} args - the parameters passed in when the event is trigger
+     * @param {string} eventType - the event name
+     * @abstract
+     * */
+    execListener(listener,args,eventType){
+        return listener(...args);
+    };
+};
+
+// @private
+
+/**
+ * @param {string} type
+ * @param {PeAEvent.Event} event
+ * */
+PeAEvent.prototype.addEvent =function(type,event){
+    if(!(type in this._events)){
+        this._events[type] =[];
+    };
+    var index =this._events[type].findIndex(
+        e=>e.getOrigin()===event.getOrigin()
+    );
+    if(~index){//if repeat, update event obj
+        this._events[type][index] =event;
+    }else{
+        this._events[type].push(event);
+    };
+};
+/**
+ * @param {string} type
+ * @param {PeAEvent.Event} event
+ * */
+PeAEvent.prototype.removeEvent =function(type,event){
+    if(!(type in this._events)){
+        return false;
+    };
+    if(event.getOrigin() ==='*'){
+        if(this._events[type].length!==0){
+            delete this._events[type];
+            return true;
+        }else{
+            return false;
+        }
+    };
+    var index =this._events[type].findIndex(
+        e=>e.getOrigin()===event.getOrigin()
+    );
+    if(~index){
+        this._events[type].splice(index,1);
+        return true;
+    };
+    return false;
+};
+
+
+
+
+
+PeAEvent.Event =class{
+    constructor(originFn,execFn=originFn){
+        this.originFn =originFn;
+        this.execFn   =execFn;
+    };
+    getOrigin(){
+        return this.originFn;
+    };
+    getExec(){
+        return this.execFn;
+    };
+    setExec(fn){
+        return this.execFn =fn;
+    };
+};
+
+if(typeof module!=='undefined')module.exports=PeAEvent;
---
+++ test/PeAEvent.test.js
@@ -0,0 +1,400 @@
+const Sinon =require('sinon');
+const Assert =require('chai').assert;
+const PeAEvent =require('../src/PeAEvent.js');
+
+//private interface
+
+/**@param {PeAEvent} peaEvent*/
+const getEventRecord =function(peaEvent){
+    return peaEvent._events;
+};
+
+describe('PeAEvent#constructor',function(){
+    it('should throw an error without extend',function(){
+        Assert.throws(function(){
+            new PeAEvent();
+        },TypeError);
+    });
+    it('if be extended, should not throw error',function(){
+        Assert.doesNotThrow(function(){
+            new class extends PeAEvent{};
+        },TypeError);
+    });
+    it('input true can be used as tools without throw error',function(){
+        var willThrowError =[false,,undefined,null,0,1,''];
+        var willWroks =[true];
+
+        willThrowError.forEach(value=>{
+            Assert.throws(function(){
+                new PeAEvent(value);
+            },TypeError);
+        });
+        willWroks.forEach(value=>{
+            Assert.doesNotThrow(function(){
+                new PeAEvent(value);
+            },TypeError);
+        });
+    });
+});
+describe('PeAEvent#on',function(){
+    it('bind event should be directly reflected on internal property',function(){
+        const PE =new PeAEvent(true);
+
+        PE.on('click',console.log);
+        Assert.lengthOf(getEventRecord(PE).click,1);
+        Assert.equal(getEventRecord(PE).click[0].getOrigin(),console.log);
+
+        PE.on('click',console.error);
+        Assert.lengthOf(getEventRecord(PE).click,2);
+        Assert.equal(getEventRecord(PE).click[1].getOrigin(),console.error);
+
+        getEventRecord(PE).click.forEach(function(e){
+            Assert.instanceOf(e ,PeAEvent.Event);
+        });
+    });
+    it('bind event with {once:true} should wrap listener',function(){
+        const PE =new PeAEvent(true);
+
+        PE.on('click',console.log,{once:true});
+        Assert.equal    (getEventRecord(PE).click[0].getOrigin(),console.log);
+        Assert.notEqual (getEventRecord(PE).click[0].getExec()  ,console.log);
+
+        getEventRecord(PE).click.forEach(function(e){
+            Assert.instanceOf(e ,PeAEvent.Event);
+        });
+    });
+    it('bind event with {once:true} should exec once',function(){
+        const PE =new PeAEvent(true);
+
+        PE.on('click',console.log,{once:true});
+        Assert.lengthOf(getEventRecord(PE).click ,1);
+
+        getEventRecord(PE).click[0].getExec()();
+        Assert.lengthOf(getEventRecord(PE).click ,0);
+
+    });
+    it('auto filter repeat',function(){
+        const PE =new PeAEvent(true);
+
+        PE.on('click',console.log);
+        Assert.lengthOf(getEventRecord(PE).click,1);
+
+        PE.on('click',console.log);
+        Assert.lengthOf(getEventRecord(PE).click,1);
+        Assert.equal(getEventRecord(PE).click[0].getOrigin(),console.log);
+        Assert.equal(getEventRecord(PE).click[0].getExec(),console.log);
+
+        PE.on('click',console.log,{once:true});
+        Assert.lengthOf(getEventRecord(PE).click,1);
+        Assert.equal(getEventRecord(PE).click[0].getOrigin(),console.log);
+        Assert.notEqual(getEventRecord(PE).click[0].getExec(),console.log);
+
+    });
+    it('defense abnormal call',function(){
+        const PE =new PeAEvent(true);
+
+        Assert.throws(function(){
+            PE.on(function(){},'click');
+        },TypeError);
+
+        Assert.throws(function(){
+            PE.on(function(){});
+        },TypeError);
+
+        Assert.throws(function(){
+            PE.on('click');
+        },TypeError);
+
+    });
+});
+describe('PeAEvent#one',function(){
+    it('in fact, it\'s alias to PeAEvent#one',function(){
+        const PE =new PeAEvent(true);
+
+        var spy =Sinon.spy(PE,'on');
+
+        PE.one('click',console.log,{other:'options',once:false});
+
+        Assert(
+            spy.withArgs('click',console.log,{other:'options',once:true}).called
+        );
+    });
+});
+describe('PeAEvent#off',function(){
+    it('call with origin listener',function(){
+        const PE =new PeAEvent(true);
+        getEventRecord(PE).click =[
+            new PeAEvent.Event(console.log),
+            new PeAEvent.Event(console.warn,console.error),
+        ];
+        Assert.lengthOf(getEventRecord(PE).click ,2);
+        PE.off('click',console.log);
+        Assert.lengthOf(getEventRecord(PE).click ,1);
+        Assert.equal(getEventRecord(PE).click[0].getOrigin() ,console.warn);
+        Assert.equal(getEventRecord(PE).click[0].getExec()   ,console.error);
+        PE.off('click',console.error);
+        Assert.lengthOf(getEventRecord(PE).click ,1);
+        PE.off('click',console.warn);
+        Assert.lengthOf(getEventRecord(PE).click ,0);
+    });
+    it('call with *',function(){
+        const PE =new PeAEvent(true);
+        getEventRecord(PE).click =[
+            new PeAEvent.Event(console.log),
+            new PeAEvent.Event(console.warn,console.error),
+        ];
+        Assert.lengthOf(getEventRecord(PE).click ,2);
+        PE.off('click','*');
+        Assert.notExists(getEventRecord(PE).click);
+    });
+});
+describe('PeAEvent#wait',function(){
+    it('wait a event',async function(){
+        const PE =new PeAEvent(true);
+
+        var flow =[1];
+
+        setTimeout(function(){
+            flow.push(2);
+            Assert.lengthOf(getEventRecord(PE).click,1);
+            getEventRecord(PE).click[0].getExec()();
+            flow.push(3);
+        });
+
+        await PE.wait('click');
+        flow.push(4);
+
+        Assert.deepEqual(flow,[1,2,3,4]);
+
+    });
+    it('wait a event with checker',async function(){
+        const PE =new PeAEvent(true);
+
+        var flow =[1];
+
+        setTimeout(function(){
+            flow.push(2);
+            Assert.lengthOf(getEventRecord(PE).click,1);
+            getEventRecord(PE).click[0].getExec()();
+            flow.push(3);
+        });
+        setTimeout(function(){
+            flow.push(4);
+            Assert.lengthOf(getEventRecord(PE).click,1);
+            getEventRecord(PE).click[0].getExec()(4);
+            flow.push(5);
+        });
+
+        await PE.wait('click',id=>id===4);
+        flow.push(6);
+
+        Assert.deepEqual(flow,[1,2,3,4,5,6]);
+
+    });
+    it('wait a *, depend on PeAEvent#execEventAll',async function(){
+        const PE =new PeAEvent(true);
+
+        var flow =[1];
+
+        setTimeout(function(){
+            flow.push(2);
+            PE.execEventAll('click');
+            flow.push(3);
+        });
+
+        await PE.wait('*');
+        flow.push(4);
+
+        Assert.deepEqual(flow,[1,2,3,4]);
+
+    });
+    it('wait a * with checker, depend on PeAEvent#execEventAll',async function(){
+        const PE =new PeAEvent(true);
+
+        var flow =[1];
+
+        setTimeout(function(){
+            flow.push(2);
+            PE.execEventAll('click',[2]);
+            flow.push(3);
+        });
+        setTimeout(function(){
+            flow.push(4);
+            PE.execEventAll('click',[4]);
+            flow.push(5);
+        });
+
+        await PE.wait('*',id=>id===4);
+        flow.push(6);
+
+        Assert.deepEqual(flow,[1,2,3,4,5,6]);
+
+    });
+
+});
+describe('PeAEvent#execEventAll',function(){
+    it('will call PeAEvent#execListener with PeAEvent.Event#getExec',function(){
+        const PE =new PeAEvent(true);
+
+        var listener =Sinon.spy();
+        var pee =new PeAEvent.Event(console.log,listener);
+        getEventRecord(PE).click =[pee];
+
+        Sinon.spy(PE ,'execListener');
+
+        var args =[Symbol()];
+        PE.execEventAll('click',args);
+
+        Assert(PE.execListener.withArgs(pee.getExec() ,args ,'click').calledOnce);
+        Assert(pee.getExec().withArgs(...args).calledOnce);
+
+    });
+    it('aways return a Promise',async function(){
+        const PE =new PeAEvent(true);
+
+        Assert.instanceOf(PE.execEventAll('click'),Promise);
+        Assert.instanceOf(PE.execEventAll('*')    ,Promise);
+        getEventRecord(PE).click=[];
+        Assert.instanceOf(PE.execEventAll('click'),Promise);
+        Assert.instanceOf(PE.execEventAll('*')    ,Promise);
+        getEventRecord(PE).click=[
+            new PeAEvent.Event(()=>{})
+        ];
+        Assert.instanceOf(PE.execEventAll('click'),Promise);
+        Assert.instanceOf(PE.execEventAll('*')    ,Promise);
+
+    });
+    it('receive listener return value',async function(){
+        const PE =new PeAEvent(true);
+
+        var flow =[1];
+
+        getEventRecord(PE).click=[
+            new PeAEvent.Event(function(val){
+                flow.push(val);
+                flow.push(2);
+                return 3;
+            })
+        ];
+
+        flow.push(4);
+
+        flow.push(
+            await PE.execEventAll('click',[5])
+        );
+
+        flow.push(6);
+
+        Assert.deepEqual(flow,[1,4,5,2,3,6]);
+
+    });
+    it('receive listener return fastest resolved promises value',async function(){
+        const PE =new PeAEvent(true);
+
+        var flow =[1];
+
+        getEventRecord(PE).click=[
+            new PeAEvent.Event(function(val){
+                flow.push(val);
+                flow.push(2);
+                return new Promise(resolve=>setTimeout(resolve.bind(null,3),20));
+            }),
+            new PeAEvent.Event(function(val){
+                flow.push(val);
+                flow.push(4);
+                return new Promise(resolve=>setTimeout(resolve.bind(null,5),10));
+            }),
+        ];
+
+        flow.push(6);
+
+        flow.push(
+            await PE.execEventAll('click',[7])
+        );
+
+        flow.push(8);
+
+        Assert.deepEqual(flow,[1,6,7,2,7,4,5,8]);
+
+    });
+});
+describe('PeAEvent other',function(){
+    it('PeAEvent#reset',function(){
+        const PE =new PeAEvent(true);
+        getEventRecord(PE).click =[
+            new PeAEvent.Event(console.log),
+            new PeAEvent.Event(console.warn),
+        ];
+        getEventRecord(PE).dbclick =[
+            new PeAEvent.Event(console.error),
+        ];
+        Assert.notDeepEqual(getEventRecord(PE),{});
+        PE.reset();
+        Assert.deepEqual(getEventRecord(PE),{});
+    });
+    it('PeAEvent#has',function(){
+        const PE =new PeAEvent(true);
+        Assert.strictEqual(PE.has('click'),0);
+        getEventRecord(PE).dbclick =[];
+        Assert.strictEqual(PE.has('click'),0);
+        getEventRecord(PE).click =[
+            new PeAEvent.Event(console.log),
+            new PeAEvent.Event(console.warn),
+        ];
+        Assert.strictEqual(PE.has('click'),2);
+        getEventRecord(PE).click.push(
+            new PeAEvent.Event(console.error)
+        );
+        Assert.strictEqual(PE.has('click'),3);
+    });
+});
+describe('comprehensive test',function(){
+    it('on/one/off/execEventAll',async function(){
+        const PE =new PeAEvent(true);
+        var listener1 =null;
+        var listener2 =null;
+
+        listener1 =Sinon.spy();
+        listener2 =Sinon.spy();
+
+        PE.on('click' ,listener1);
+        PE.one('*'    ,listener2);
+
+        var args1 =[Symbol()];
+        await PE.execEventAll('click',args1);
+
+        Assert(listener1.withArgs(...args1).called);
+        Assert(listener2.withArgs(...args1).called);
+
+        await PE.execEventAll('click',args1);
+        Assert.strictEqual(listener1.callCount ,2);
+        Assert.strictEqual(listener2.callCount ,1);
+
+        PE.off('click',listener1);
+        await PE.execEventAll('click',args1);
+        Assert.strictEqual(listener1.callCount ,2);
+        Assert.strictEqual(listener2.callCount ,1);
+
+        var sym =Symbol();
+        listener1 =Sinon.spy(function(){
+            return Promise.resolve(sym);
+        });
+        listener2 =Sinon.spy(function(){});
+        PE.one('click',listener1);
+        PE.on ('click',listener2);
+        Assert.strictEqual((await PE.execEventAll('click',args1)),sym);
+
+    });
+    it('wait/off*/execEventAll',async function(){
+        const PE =new PeAEvent(true);
+
+        setTimeout(function(){
+            PE.off('click','*');
+            PE.execEventAll('click');
+        });
+
+        PE.wait('click').then(function(){
+            Assert(false ,'off(eventType ,*) should remove waiter');
+        });
+    });
+});
